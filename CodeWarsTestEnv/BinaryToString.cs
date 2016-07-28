b18c2dd608e7ea7e5af16f76480ff1c0db6c533f
Dragan Skorosavljević <Dragan.Skorosavljevic@in2.eu>
2016-07-28 14:20:44 +0200

    solved BinaryToString

--- CodeWarsTestEnv/BinaryToString.cs
+++ CodeWarsTestEnv/BinaryToString.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace CodeWarsTestEnv
 {
-    /*  Description:
-    --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    /*  Binary to Text (ASCII) Conversion
+    --------------------------------------------------------------------------------------------------------------------------------------------------------------------       
         Write a function that takes in a binary string and returns the equivalent decoded text (the text is ASCII encoded).
         Each 8 bits on the binary string represent 1 character on the ASCII table.
         The input string will always be a valid binary string.
@@ -19,19 +19,18 @@ namespace CodeWarsTestEnv
     {
         public static string MySolution(string binary)
         {
-            var text = new StringBuilder();
             int i = 0;
-            while (i < binary.Length) {
-                var strby = binary.Skip(i).Take(8).Select( x => Convert.ToSingle(x));
-                //var str = Encoding.ASCII.GetString(strby, 0, strby.Length);
-                //text.Append(str);
-                i += 8;
+            byte[] byteArr = new byte[binary.Length/8];
+            while ((i *8) < binary.Length) {
+                var byteRep = Convert.ToInt32(binary.Substring((i * 8), 8), 2);
+                Byte.TryParse(byteRep.ToString(), out byteArr[i]);                         
+                i++;
             }
+            return Encoding.ASCII.GetString(byteArr);
+        }
 
-            var stsr = Encoding.ASCII.GetString(new byte[8] {0,1,0,0,1,0,0,0});
-                       
-
-            return text.ToString();
+        public static string TheBest(string binary) {
+            return Encoding.ASCII.GetString( Enumerable.Range(0, binary.Length / 8).Select(i => Convert.ToByte(binary.Substring(8 * i, 8), 2)).ToArray());
         }
     }
 }
